6f78a1e6ca30acf95dffbceb52050b91c35a0062
Julia060505 <jcarneironovaes@gmail.com>
2022-07-11 22:40:36 -0300

    Aula Condicional Avançado

---
+++ Códigos/AulasGravadas/booleano.js
@@ -0,0 +1,13 @@
+let datas = "Natal";
+
+let tenhoChocolate = datas === "Páscoa"; // false
+let souNoFimDoAno = datas === "Natal"; // true
+
+// console.log(souNoFimDoAno)
+
+let subtracao = 20-15;
+
+let resultadoValido = subtracao > 5; //false
+let resultadoValido2 = subtracao >= 5; // true
+
+// console.log(resultadoValido)
---
+++ Códigos/AulasGravadas/ternario.js
@@ -0,0 +1,15 @@
+let euSou = true;
+
+let quemSouEu = euSou ? "Aluna do Campinho" : "Professora do Campinho"
+
+// ? - Atribui o valor de verdadeiro para a variável
+// : - Atribui o valor de falso para a variável
+
+// console.log(quemSouEu)
+
+let minhaSenha = 51;
+let novaSenha = 99;
+
+let confirmacao = minhaSenha === novaSenha ? "Senha antiga" : "Senha válida"
+
+// console.log(confirmacao)
