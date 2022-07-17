2594d21c832ab8dc50e3647156dd0c6fc4aeb96e
Julia060505 <jcarneironovaes@gmail.com>
2022-07-17 14:24:47 -0300

    Acerto de atividades em sala

--- Códigos/ExEMsala/13Jul,Ativ4.js
+++ Códigos/ExEMsala/13Jul,Ativ4.js
@@ -5,12 +5,12 @@ let = maça = parseInt(rs.question('Quantas macas voce comprou?'))
 //  QUESTÃO 4 (13/07)
 
 if( maça < 12){
-
-    console.log("Cada maça tem o custo de R$0,30.")
+    let m1 = maça * 0.3
+    console.log("Cada maça tem o custo de R$0,30.Sua conta ficou R$:", m1)
 
 }else if (maça >= 12){
-
-    console.log("Cada maça tem o custo de R$0,25.")
+    let m2 = maça * 0.25
+    console.log("Cada maça tem o custo de R$0,25.Sua conta ficou R$:", m2)
 }else{
     console.log("Valor não reconhecido")
 }
--- Códigos/ExEMsala/13Jul,Ativ5.js
+++ Códigos/ExEMsala/13Jul,Ativ5.js
@@ -1,6 +1,26 @@
-let rs = require ('readline-sync');
+const readline = require ('readline');
 
-let = valor = parseInt(rs.question('Digite 3 valores inteiros:'))
+const face = readline.createInterface({input:process.stdin,output:process.stdout});
 
 // QUESTÃO 5 (13/07)
 
+let crescer = []
+
+face.question("Primeiro numero:", (n1) => {
+
+    face.question("Segundo numero:", (n2) => {
+
+        face.question("Terceiro numero:", (n3) => {
+             
+            crescer.push(n1)
+            crescer.push(n2)
+            crescer.push(n3)
+
+            crescer.sort()
+
+            console.log(crescer)
+        })
+    })
+})
+
+// fiz o e  xercício com base no código da Paucia, pois tive dúvida e só entendi observando
